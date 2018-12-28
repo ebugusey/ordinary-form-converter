c253b663f8c01cced7d9404101ba959585ecfbe3
MaxMauz <64520728+MaxMauz@users.noreply.github.com>
2018-12-28 17:32:02 +0400

    [REQ] #55639 Добавлен класс RadioButtonDecoration

---
+++ src/OFP/ObjectModel/FormElements/RadioButtonDecoration.cs
@@ -0,0 +1,19 @@
+using OFP.ObjectModel.Platform;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OFP.ObjectModel.FormElements
+{
+    class RadioButtonDecoration
+    {
+
+        public Border Border { get; set; }
+        public Font Font { get; set; }
+        public Color BackColor { get; set; }
+        public Color BorderColor { get; set; }
+        public Color TextColor { get; set; }
+        public bool IsTransparent { get; set; }
+
+    }
+}
