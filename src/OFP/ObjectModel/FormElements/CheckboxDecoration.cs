328ef92a4dd5f8e497c6bcc96f7a1efa2be8d646
Гуреев Егор (Gureev_EO) <Gureev_EO@directum.ru>
2018-12-28 16:35:34 +0400

    [REQ] #55639 Добавлена модель Checkbox

---
+++ src/OFP/ObjectModel/FormElements/CheckboxDecoration.cs
@@ -0,0 +1,14 @@
+using OFP.ObjectModel.Platform;
+
+namespace OFP.ObjectModel.FormElements
+{
+    public class CheckboxDecoration
+    {
+        public Border Border { get; set; }
+        public Font Font { get; set; }
+        public Color BackColor { get; set; }
+        public Color BorderColor { get; set; }
+        public Color TextColor { get; set; }
+        public bool Transparent { get; set; }
+    }
+}
