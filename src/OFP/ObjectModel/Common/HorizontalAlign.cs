a3931986a13239aafc13a0b1b6693fe68bb5bcd0
Гуреев Егор (Gureev_EO) <Gureev_EO@directum.ru>
2018-12-28 15:46:28 +0400

    [REQ] #55639 Добавлено перечисление HorizontalAlign

---
+++ src/OFP/ObjectModel/Common/HorizontalAlign.cs
@@ -0,0 +1,11 @@
+namespace OFP.ObjectModel.Common
+{
+    public enum HorizontalAlign
+    {
+        Auto = 0,
+        Left = 1,
+        Justify = 2,
+        Right = 3,
+        Center = 4
+    }
+}
