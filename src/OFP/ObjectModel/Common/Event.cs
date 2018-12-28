67e412ec35db82f25a34d64a2f63bc702a3dbb9c
Гуреев Егор (Gureev_EO) <Gureev_EO@directum.ru>
2018-12-28 16:48:26 +0400

    [REQ] #55639 Классу Event добавлено ограничение на тип T

--- src/OFP/ObjectModel/Common/Event.cs
+++ src/OFP/ObjectModel/Common/Event.cs
@@ -1,6 +1,6 @@
 namespace OFP.ObjectModel.Common
 {
-    public class Event<T>
+    public class Event<T> where T : Enum
     {
         public string HeandlerName { get; set; }
         public T Type { get; set; }
