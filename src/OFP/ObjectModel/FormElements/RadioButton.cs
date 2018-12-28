ee05a12a96ae194de5abcc8c4df0f826d5ff9cb8
MaxMauz <64520728+MaxMauz@users.noreply.github.com>
2018-12-28 17:28:50 +0400

    [REQ] #55639 Добавлено класс RadioButton

---
+++ src/OFP/ObjectModel/FormElements/RadioButton.cs
@@ -0,0 +1,19 @@
+using OFP.ObjectModel.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OFP.ObjectModel.FormElements
+{
+    public class RadioButton : Element
+    {
+
+        public bool IsDefaultControl { get; set; }
+        public Event<RadioButton> Events { get; set; }
+        public HorizontalAlign HorizontalAlign { get; set; }
+        public TitleLocation TitleLocation { get; set; }
+        public VerticalAlign VerticalAlign { get; set; }
+
+
+    }
+}
