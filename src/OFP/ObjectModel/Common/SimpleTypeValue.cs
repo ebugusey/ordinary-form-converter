8f207251cf8e0a924539eaf0e43fc0cf574fa971
MaxMauz <64520728+MaxMauz@users.noreply.github.com>
2018-12-28 17:51:32 +0400

    [REQ] #55639 Добавлен класс SimpleTypeValue

---
+++ src/OFP/ObjectModel/Common/SimpleTypeValue.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OFP.ObjectModel.Common
+{
+    public class SimpleTypeValue
+    {
+
+        public SimpleType Type { get; set; }
+        public string Value { get; set; }
+
+    }
+}
