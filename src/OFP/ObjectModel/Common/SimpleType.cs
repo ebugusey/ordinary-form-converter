1b51c4f2992990e837ee24ff6416a2ba533ef268
MaxMauz <64520728+MaxMauz@users.noreply.github.com>
2018-12-28 17:24:51 +0400

    [REQ] #55639 Добавлено перечисление SimpleType

---
+++ src/OFP/ObjectModel/Common/SimpleType.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OFP.ObjectModel.Common
+{
+    enum SimpleType
+    {
+
+        Unified = 0,
+        String = 1,
+        Number = 2,
+        Boolean = 3,
+        Data = 4,
+        Reference = 5
+        
+    }
+}
