f5429376481622f442ef31d1a199e8132c670ba0
ebugusey <ebugusey@users.noreply.github.com>
2019-01-09 15:28:46 +0400

    [REQ] #55639 Добавлена объектная модель надписи

---
+++ src/OFP/ObjectModel/Common/ScrollingTextMode.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OFP.ObjectModel.Common
+{
+    public enum ScrollingTextMode
+    {
+        DontUse = 0,
+        VerySlow = 1,
+        Slow = 2,
+        Normal = 3,
+        Fast = 4,
+        VeryFast = 5,
+    }
+}
--- src/OFP/ObjectModel/FormElements/LabelDecoration.cs
+++ src/OFP/ObjectModel/FormElements/LabelDecoration.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using OFP.ObjectModel.Common;
 using OFP.ObjectModel.Platform.Borders;
 using OFP.ObjectModel.Platform.Colors;
 using OFP.ObjectModel.Platform.Fonts;
+using OFP.ObjectModel.Platform.Pictures;
 
 namespace OFP.ObjectModel.FormElements
 {
@@ -14,6 +16,9 @@ namespace OFP.ObjectModel.FormElements
         public Color BackColor { get; set; }
         public Color BorderColor { get; set; }
         public Color TextColor { get; set; }
-
+        public Picture Picture { get; set; }
+        public PicturePosition PicturePosition { get; set; }
+        public PictureSize PictureSize { get; set; }
+        public bool IsTransparent { get; set; }
     }
 }
