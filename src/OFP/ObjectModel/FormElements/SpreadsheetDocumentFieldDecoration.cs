e756368db674867c3c81a1807b04ea71487d4653
MaxMauz <64520728+MaxMauz@users.noreply.github.com>
2019-01-09 10:33:35 +0400

    [REQ] #55639 Добавлен класс SpreadsheetDocumentFieldDecoration

---
+++ src/OFP/ObjectModel/FormElements/SpreadsheetDocumentFieldDecoration.cs
@@ -0,0 +1,15 @@
+using OFP.ObjectModel.Platform;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OFP.ObjectModel.FormElements
+{
+    class SpreadsheetDocumentFieldDecoration
+    {
+
+        public Border Border { get; set; }
+        public Color BorderColor { get; set; }
+
+    }
+}
