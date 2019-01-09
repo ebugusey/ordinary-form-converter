8ca8c424f5cf08057f76c490bac355f18f351cb6
MaxMauz <64520728+MaxMauz@users.noreply.github.com>
2019-01-09 10:34:35 +0400

    [REQ] #55639 Добавлен класс SpreadsheetDocumentField

---
+++ src/OFP/ObjectModel/FormElements/SpreadsheetDocumentField.cs
@@ -0,0 +1,19 @@
+using OFP.ObjectModel.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OFP.ObjectModel.FormElements
+{
+    class SpreadsheetDocumentField : Element
+    {
+
+        public bool EnableDrag { get; set; }
+        public bool EnableStartDrag { get; set; }
+        public Event<SpreadsheetDocumentField> Events { get; set; }
+        public bool HorizontalScrollBar { get; set; }
+        public SpreadsheetDocumentSelectionShowModeType ShowSelection { get; set; }
+        public bool VerticalScrollBar { get; set; }
+
+    }
+}
