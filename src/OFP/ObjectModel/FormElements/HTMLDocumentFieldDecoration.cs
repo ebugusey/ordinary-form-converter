27960ebced003314faa3c6a9068a616b683bd1f7
MaxMauz <64520728+MaxMauz@users.noreply.github.com>
2019-01-22 09:36:02 +0400

    [REQ] #55639 Добавлен класс HTMLDocumentFieldDecoration

---
+++ src/OFP/ObjectModel/FormElements/HTMLDocumentFieldDecoration.cs
@@ -0,0 +1,16 @@
+using OFP.ObjectModel.Platform;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OFP.ObjectModel.FormElements
+{
+    class HTMLDocumentFieldDecoration
+    {
+
+        public Border Border { get; set; }
+        public Font Font { get; set; }
+        public Color BorderColor { get; set; }
+
+    }
+}
