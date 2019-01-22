ccdb365f5e1d3a8bd55ff83cdfc2ce55a8480254
MaxMauz <64520728+MaxMauz@users.noreply.github.com>
2019-01-22 15:51:06 +0400

    [REQ] #55639 Добавлено перчисление TableBoxRowInputMode

---
+++ src/OFP/ObjectModel/Common/TableBoxRowInputMode.cs
@@ -0,0 +1,10 @@
+namespace OFP.ObjectModel.Common
+{
+    public enum TableBoxRowInputMode
+    {
+        EndOfList = 0,
+        EndOfWindow = 1,
+        AfterCurrentRow = 2,
+        BeforeCurrentRow = 3
+    }
+}
