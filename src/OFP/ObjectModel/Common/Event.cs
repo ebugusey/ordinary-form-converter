511c0a40dea43496924b8fa6e05aa7fef1cdedac
Гуреев Егор (Gureev_EO) <Gureev_EO@directum.ru>
2019-01-22 17:25:32 +0400

    [REQ] #55639 Снято ограничение с типа события
    Enum не поддерживается как ограничитель.
    https://github.com/dotnet/roslyn/issues/18369

--- src/OFP/ObjectModel/Common/Event.cs
+++ src/OFP/ObjectModel/Common/Event.cs
@@ -1,6 +1,6 @@
 namespace OFP.ObjectModel.Common
 {
-    public class Event<T> where T : Enum
+    public class Event<T>
     {
         public string HandlerName { get; set; }
         public T Type { get; set; }
