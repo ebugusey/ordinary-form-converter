6e1125d2d29298ef14c5e5173f32f2b86abec7ce
Гуреев Егор (Gureev_EO) <Gureev_EO@directum.ru>
2019-01-22 16:03:10 +0400

    [REQ] #55639 Добавлена модель Splitter

---
+++ src/OFP/ObjectModel/FormElements/SplitterDecoration.cs
@@ -0,0 +1,12 @@
+using OFP.ObjectModel.Platform;
+
+namespace OFP.ObjectModel.FormElements
+{
+    public class SplitterDecoration
+    {
+        public Border Border { get; set; }
+        public Color BackColor { get; set; }
+        public Color BorderColor { get; set; }
+        public bool IsTransparent { get; set; }
+    }
+}
