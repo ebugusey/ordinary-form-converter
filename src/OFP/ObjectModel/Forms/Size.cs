e8d0574c9da055f6068af85bc73fb8333dd20308
MaxMauz <64520728+MaxMauz@users.noreply.github.com>
2019-01-23 09:53:51 +0400

    [REQ] #55639 Добавлен класс Size

---
+++ src/OFP/ObjectModel/Forms/Size.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OFP.ObjectModel.Forms
+{
+    public class Size
+    {
+        public uint Width { get; set; }
+        public uint Height { get; set; }
+    }
+}
