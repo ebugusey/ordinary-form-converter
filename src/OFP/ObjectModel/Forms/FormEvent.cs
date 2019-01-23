8125d06c5e3770d61b710412f48116316e34d122
MaxMauz <64520728+MaxMauz@users.noreply.github.com>
2019-01-23 16:34:31 +0400

    [REQ] #55639 Добавлено перечисление FormEvent

---
+++ src/OFP/ObjectModel/Forms/FormEvent.cs
@@ -0,0 +1,18 @@
+namespace OFP.ObjectModel.Forms
+{
+    public enum FormEvent
+    {
+        BeforeOpen = 70000,
+        OnOpen = 70001,
+        BeforeClose = 70002,
+        OnClose = 70003,
+        ChoiceProcessing = 70004,
+        ObjectActivationProcessing = 70005,
+        NewObjectWriteProcessing = 70006,
+        NotificationProcessing = 70007,
+        OnReopen = 70008,
+        RefreshDisplay = 70009,
+        ExternalEvent = 70010,
+        FillCheckProcessing = 70011
+    }
+}
