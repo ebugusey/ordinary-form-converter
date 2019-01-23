8310a3df28ec22337e9bcdc33558dba63b6bc496
MaxMauz <64520728+MaxMauz@users.noreply.github.com>
2019-01-23 17:04:37 +0400

    [REQ] #55639 Добавлены директории using в GroupBoxDecoration

--- src/OFP/ObjectModel/FormElements/GroupBoxDecoration.cs
+++ src/OFP/ObjectModel/FormElements/GroupBoxDecoration.cs
@@ -1,4 +1,7 @@
 using OFP.ObjectModel.Platform;
+using OFP.ObjectModel.Platform.Borders;
+using OFP.ObjectModel.Platform.Colors;
+using OFP.ObjectModel.Platform.Fonts;
 
 namespace OFP.ObjectModel.FormElements
 {
