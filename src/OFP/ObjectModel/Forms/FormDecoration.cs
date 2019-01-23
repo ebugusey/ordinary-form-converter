82e7a917b61b0f34aade9509675b5d2749556bd9
MaxMauz <64520728+MaxMauz@users.noreply.github.com>
2019-01-23 09:55:22 +0400

    [REQ] #55639 Добавлен класс FormDecoration

---
+++ src/OFP/ObjectModel/Forms/FormDecoration.cs
@@ -0,0 +1,13 @@
+using OFP.ObjectModel.Platform;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OFP.ObjectModel.Forms
+{
+    public class FormDecoration
+    {
+        public string Style { get; set; }
+        public Picture CaptionPicture { get; set; }
+    }
+}
