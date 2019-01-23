84d2ce5e4d6ba0024ce023ad1e125a97c9887bdb
MaxMauz <64520728+MaxMauz@users.noreply.github.com>
2019-01-23 16:30:59 +0400

    [REQ] #55639 Добавлено перечисление SpreadsheetDocumentFieldEvent

---
+++ src/OFP/ObjectModel/FormElements/SpreadsheetDocumentFieldEvent.cs
@@ -0,0 +1,14 @@
+namespace OFP.ObjectModel.FormElements
+{
+    public enum SpreadsheetDocumentFieldEvent
+    {
+        Selection = 0,
+        DetailProcessing = 1,
+        OnActivateArea = 2,
+        DragStart = 3,
+        DragCheck = 4,
+        DragEnd = 5,
+        Drag = 6,
+        OnChangeAreaContent = 7
+    }
+}
