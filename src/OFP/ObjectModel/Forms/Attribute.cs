372eeed64016461872c099e040a9538366c2340d
MaxMauz <64520728+MaxMauz@users.noreply.github.com>
2019-01-23 09:54:51 +0400

    [REQ] #55639 Добавлен класс Attribute

---
+++ src/OFP/ObjectModel/Forms/Attribute.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OFP.ObjectModel.Forms
+{
+    public class Attribute
+    {
+        public string Name { get; set; }
+        public bool FillCheck { get; set; }
+        public bool SavedData { get; set; }
+    }
+}
