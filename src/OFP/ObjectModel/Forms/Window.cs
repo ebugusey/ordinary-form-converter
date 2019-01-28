d5d4dedd25452615f38a091ed4161710afad570e
ebugusey <ebugusey@users.noreply.github.com>
2019-01-28 14:33:38 +0400

    [TEC] Исправлена опечатка
    Имена свойств должны начинаться с прописной буквы.

--- src/OFP/ObjectModel/Forms/Window.cs
+++ src/OFP/ObjectModel/Forms/Window.cs
@@ -6,7 +6,7 @@ namespace OFP.ObjectModel.Forms
 {
     public class Window
     {
-        public WindowStateVariant windowState { get; set; }
+        public WindowStateVariant WindowState { get; set; }
         public bool AllowClose { get; set; }
         public bool DesktopMode { get; set; }
         public WindowSizeChange SizeChange { get; set; }
