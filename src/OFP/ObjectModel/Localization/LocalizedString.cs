1fa6850bb351c3d9ce0acfb74bc9ff97f0d359bc
ebugusey <ebugusey@users.noreply.github.com>
2019-01-28 16:33:27 +0400

    [REQ] #55639 Убрано все лишнее из LocalizedString
    LocalizedString сделан наподобие класса Events. В этом классе не нужна полная
    реализация `IDictionary<,>`, не смотря на то, что словарь используется в качестве
    хранилища значений.

--- src/OFP/ObjectModel/Localization/LocalizedString.cs
+++ src/OFP/ObjectModel/Localization/LocalizedString.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace OFP.ObjectModel.Localization
 {
-    public class LocalizedString : IDictionary<Locale, string>
+    public class LocalizedString : IEnumerable<KeyValuePair<Locale, string>>
     {
         private readonly IDictionary<Locale, string> _strings;
 
@@ -15,64 +15,37 @@ namespace OFP.ObjectModel.Localization
             get => _strings[key];
             set => _strings[key] = value;
         }
-        public ICollection<Locale> Keys => _strings.Keys;
-        public ICollection<string> Values => _strings.Values;
+
         public int Count => _strings.Count;
-        public bool IsReadOnly => _strings.IsReadOnly;
 
         public LocalizedString()
         {
             _strings = new Dictionary<Locale, string>();
         }
 
-        public void Add(Locale key, string value)
+        public void Add(Locale locale, string value)
         {
-            _strings.Add(key, value);
+            _strings.Add(locale, value);
         }
 
-        public void Add(KeyValuePair<Locale, string> item)
+        public bool HasLocale(Locale locale)
         {
-            _strings.Add(item);
+            return _strings.ContainsKey(locale);
         }
 
-        public void Clear()
+        public bool TryGetString(Locale locale, out string value)
         {
-            _strings.Clear();
+            return _strings.TryGetValue(locale, out value);
         }
 
-        public bool Contains(KeyValuePair<Locale, string> item)
+        public bool Remove(Locale locale)
         {
-            return _strings.Contains(item);
-        }
-
-        public bool ContainsKey(Locale key)
-        {
-            return _strings.ContainsKey(key);
-        }
-
-        public void CopyTo(KeyValuePair<Locale, string>[] array, int arrayIndex)
-        {
-            _strings.CopyTo(array, arrayIndex);
+            return _strings.Remove(locale);
         }
 
         public IEnumerator<KeyValuePair<Locale, string>> GetEnumerator()
         {
             return _strings.GetEnumerator();
-        }
-
-        public bool Remove(Locale key)
-        {
-            return _strings.Remove(key);
-        }
-
-        public bool Remove(KeyValuePair<Locale, string> item)
-        {
-            return _strings.Remove(item);
-        }
-
-        public bool TryGetValue(Locale key, out string value)
-        {
-            return _strings.TryGetValue(key, out value);
         }
 
         IEnumerator IEnumerable.GetEnumerator()
