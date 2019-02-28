671f3363f951181c280c97615f54d96a3a600946
MaxMauz <64520728+MaxMauz@users.noreply.github.com>
2019-02-28 16:09:12 +0400

    [REQ] #55639 Переименованы свойства в перечислении ScrollBarUse

--- src/OFP/ObjectModel/FormElements/Data/ScrollBarUse.cs
+++ src/OFP/ObjectModel/FormElements/Data/ScrollBarUse.cs
@@ -18,6 +18,6 @@ namespace OFP.ObjectModel.FormElements.Data
         /// <summary>
         /// ИспользоватьАвтоматически.
         /// </summary>
-        AutoUse = 2,
+        UseAuto = 2,
     }
 }
