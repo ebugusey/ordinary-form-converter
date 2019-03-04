5ef8b71c1fc5ff0a57ec57ae6748813a3fab9c64
MaxMauz <64520728+MaxMauz@users.noreply.github.com>
2019-03-04 16:19:34 +0400

    [REQ] #55639 Переименованы свойства в перечислении PanelEvent

--- src/OFP/ObjectModel/FormElements/Panels/PanelEvent.cs
+++ src/OFP/ObjectModel/FormElements/Panels/PanelEvent.cs
@@ -12,6 +12,6 @@ namespace OFP.ObjectModel.FormElements.Panels
         /// <summary>
         /// ПриСменеСтраницы.
         /// </summary>
-        OnCurrentPageChange = 0,
+        OnChangeCurrentPage = 0,
     }
 }
