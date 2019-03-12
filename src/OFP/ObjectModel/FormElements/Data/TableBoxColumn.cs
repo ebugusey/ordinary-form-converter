fb209ecf87b06ac7bbd44da09152c9a026dda98f
MaxMauz <64520728+MaxMauz@users.noreply.github.com>
2019-03-12 13:22:34 +0400

    [REQ] #55639 Упорядочены свойства в классе TableBoxColumn

--- src/OFP/ObjectModel/FormElements/Data/TableBoxColumn.cs
+++ src/OFP/ObjectModel/FormElements/Data/TableBoxColumn.cs
@@ -6,55 +6,16 @@ using System.Text;
 
 namespace OFP.ObjectModel.FormElements.Data
 {
+    // FIXME: Нет свойств: Формат, АктивизироватьПоУмолчанию.
     /// <summary>
     /// КолонкаТабличногоПоля.
     /// </summary>
     public class TableBoxColumn
     {
         /// <summary>
-        /// АвтоВысотаЯчейки.
+        /// Имя.
         /// </summary>
-        public bool AutoCellHeight { get; set; }
-
-        /// <summary>
-        /// АвтоОтметкаНезаполненного.
-        /// </summary>
-        public bool AutoMarkBlank { get; set; }
-
-        /// <summary>
-        /// ВысотаЯчейки.
-        /// </summary>
-        public int CellHeight { get; set; }
-
-        /// <summary>
-        /// ИзменятьПозицию.
-        /// </summary>
-        public bool CanChangePosition { get; set; }
-
-        /// <summary>
-        /// ИзменятьНастройку.
-        /// </summary>
-        public bool CanChangeSetting { get; set; }
-
-        /// <summary>
-        /// ИзменятьВидимость.
-        /// </summary>
-        public bool CanChangeVisible { get; set; }
-
-        /// <summary>
-        /// ТриСостоянияФлажка.
-        /// </summary>
-        public bool HasThreeStatesCheckBox { get; set; }
-
-        /// <summary>
-        /// ДанныеФлажка.
-        /// </summary>
-        public string CheckBoxDataPath { get; set; }
-
-        /// <summary>
-        /// ЭлементУправления.
-        /// </summary>
-        public Element Control { get; set; }
+        public Identifier Name { get; set; }
 
         /// <summary>
         /// Данные.
@@ -62,59 +23,14 @@ namespace OFP.ObjectModel.FormElements.Data
         public string DataPath { get; set; }
 
         /// <summary>
-        /// Оформление колонки.
+        /// ЭлементУправления.
         /// </summary>
-        public TableBoxColumnDecoration Decoration { get; set; }
+        public Element Control { get; set; }
 
         /// <summary>
-        /// РежимРедактирования.
+        /// ДанныеФлажка.
         /// </summary>
-        public ColumnEditMode EditMode { get; set; }
-
-        /// <summary>
-        /// Доступность.
-        /// </summary>
-        public bool Enabled { get; set; }
-
-        /// <summary>
-        /// ГоризонтальноеПоложениеВПодвале.
-        /// </summary>
-        public HorizontalAlign FooterHorizontalAlign { get; set; }
-
-        /// <summary>
-        /// ГоризонтальноеПоложениеВШапке.
-        /// </summary>
-        public HorizontalAlign HeaderHorizontalAlign { get; set; }
-
-        /// <summary>
-        /// ПодсказкаВШапке.
-        /// </summary>
-        public LocalizedString HeaderToolTip { get; set; }
-
-        /// <summary>
-        /// ГоризонтальноеПоложениеВКолонке.
-        /// </summary>
-        public HorizontalAlign HorizontalAlignInColumn { get; set; }
-
-        /// <summary>
-        /// Гиперссылка.
-        /// </summary>
-        public bool IsHyperlink { get; set; }
-
-        /// <summary>
-        /// Положение.
-        /// </summary>
-        public ColumnLocation Position { get; set; }
-
-        /// <summary>
-        /// ВыделятьОтрицательные.
-        /// </summary>
-        public bool IsMarkNegativeNumbers { get; set; }
-
-        /// <summary>
-        /// Имя.
-        /// </summary>
-        public Identifier Name { get; set; }
+        public bool CheckBoxDataPath { get; set; }
 
         /// <summary>
         /// ДанныеКартинки.
@@ -122,39 +38,19 @@ namespace OFP.ObjectModel.FormElements.Data
         public string PictureData { get; set; }
 
         /// <summary>
+        /// Доступность.
+        /// </summary>
+        public bool Enabled { get; set; }
+
+        /// <summary>
+        /// Видимость.
+        /// </summary>
+        public bool Visible { get; set; }
+
+        /// <summary>
         /// ТолькоПросмотр.
         /// </summary>
         public bool IsReadOnly { get; set; }
-
-        /// <summary>
-        /// ОтображатьИерархию.
-        /// </summary>
-        public bool HierarchyVisible { get; set; }
-
-        /// <summary>
-        /// ОтображатьВПодвале.
-        /// </summary>
-        public bool VisibleInFooter { get; set; }
-
-        /// <summary>
-        /// ОтображатьВШапке.
-        /// </summary>
-        public bool VisibleInHeader { get; set; }
-
-        /// <summary>
-        /// ИзменениеРазмера.
-        /// </summary>
-        public ColumnSizeChange SizeChangeMode { get; set; }
-
-        /// <summary>
-        /// ПропускатьПриВводе.
-        /// </summary>
-        public bool IsSkipOnInput { get; set; }
-
-        /// <summary>
-        /// ТекстПодвала.
-        /// </summary>
-        public LocalizedString FooterText { get; set; }
 
         /// <summary>
         /// ТекстШапки.
@@ -162,8 +58,113 @@ namespace OFP.ObjectModel.FormElements.Data
         public LocalizedString HeaderText { get; set; }
 
         /// <summary>
-        /// Видимость.
+        /// ТекстПодвала.
         /// </summary>
-        public bool Visible { get; set; }
+        public LocalizedString FooterText { get; set; }
+
+        /// <summary>
+        /// Гиперссылка.
+        /// </summary>
+        public bool IsHyperlink { get; set; }
+
+        /// <summary>
+        /// АвтоОтметкаНезаполненного.
+        /// </summary>
+        public bool AutoMarkBlank { get; set; }
+
+        /// <summary>
+        /// ВыделятьОтрицательные.
+        /// </summary>
+        public bool IsMarkNegativeNumbers { get; set; }
+
+        /// <summary>
+        /// ТриСостоянияФлажка.
+        /// </summary>
+        public bool HasThreeStatesCheckBox { get; set; }
+
+        /// <summary>
+        /// Положение.
+        /// </summary>
+        public ColumnLocation Position { get; set; }
+
+        /// <summary>
+        /// ПодсказкаВШапке.
+        /// </summary>
+        public LocalizedString HeaderToolTip { get; set; }
+
+        /// <summary>
+        /// ГоризонтальноеПоложениеВШапке.
+        /// </summary>
+        public HorizontalAlign HeaderHorizontalAlign { get; set; }
+
+        /// <summary>
+        /// ГоризонтальноеПоложениеВКолонке.
+        /// </summary>
+        public HorizontalAlign HorizontalAlignInColumn { get; set; }
+
+        /// <summary>
+        /// ГоризонтальноеПоложениеВПодвале.
+        /// </summary>
+        public HorizontalAlign FooterHorizontalAlign { get; set; }
+
+        /// <summary>
+        /// ОтображатьВШапке.
+        /// </summary>
+        public bool VisibleInHeader { get; set; }
+
+        /// <summary>
+        /// ОтображатьВПодвале.
+        /// </summary>
+        public bool VisibleInFooter { get; set; }
+
+        /// <summary>
+        /// ОтображатьИерархию.
+        /// </summary>
+        public bool HierarchyVisible { get; set; }
+
+        /// <summary>
+        /// РежимРедактирования.
+        /// </summary>
+        public ColumnEditMode EditMode { get; set; }
+
+        /// <summary>
+        /// ИзменениеРазмера.
+        /// </summary>
+        public ColumnSizeChange SizeChangeMode { get; set; }
+
+        /// <summary>
+        /// ИзменятьВидимость.
+        /// </summary>
+        public bool CanChangeVisible { get; set; }
+
+        /// <summary>
+        /// ИзменятьНастройку.
+        /// </summary>
+        public bool CanChangeSetting { get; set; }
+
+        /// <summary>
+        /// ИзменятьПозицию.
+        /// </summary>
+        public bool CanChangePosition { get; set; }
+
+        /// <summary>
+        /// ПропускатьПриВводе.
+        /// </summary>
+        public bool IsSkipOnInput { get; set; }
+
+        /// <summary>
+        /// ВысотаЯчейки.
+        /// </summary>
+        public int CellHeight { get; set; }
+
+        /// <summary>
+        /// АвтоВысотаЯчейки.
+        /// </summary>
+        public bool AutoCellHeight { get; set; }
+
+        /// <summary>
+        /// Оформление колонки.
+        /// </summary>
+        public TableBoxColumnDecoration Decoration { get; set; }
     }
 }
