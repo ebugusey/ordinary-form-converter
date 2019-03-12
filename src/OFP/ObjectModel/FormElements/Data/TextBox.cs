12345ef585101fbda89baa2e7f96f3d67c68f903
MaxMauz <64520728+MaxMauz@users.noreply.github.com>
2019-03-12 13:29:20 +0400

    [REQ] #55639 Упорядочены свойства в классе TextBox

--- src/OFP/ObjectModel/FormElements/Data/TextBox.cs
+++ src/OFP/ObjectModel/FormElements/Data/TextBox.cs
@@ -2,43 +2,26 @@ using OFP.ObjectModel.Common;
 
 namespace OFP.ObjectModel.FormElements.Data
 {
+    //FIXME: Нет свойств: ФиксДробнаяЧасть, РедактированиеТекста, РежимВыбораНезаполненного, РежимВыбораИзСписка.
     /// <summary>
     /// ПолеВвода.
     /// </summary>
     public class TextBox : Element
     {
-        /// <summary>
-        /// АвтоВыборНезаполненного.
-        /// </summary>
-        public bool AutoChoiceBlank { get; set; }
-
-        /// <summary>
-        /// АвтоОтметкаНезаполненного.
-        /// </summary>
-        public bool AutoMarkBlank { get; set; }
-
+        //FIXME: Надо удалить свойство.
         public bool ChoiceIncomplete { get; set; }
+        //FIXME: Надо удалить свойство.
         public bool MarkIncomplete { get; set; }
-
-        /// <summary>
-        /// ВыбиратьТип.
-        /// </summary>
-        public bool IsChooseType { get; set; }
-
-        /// <summary>
-        /// ВыделятьОтрицательные.
-        /// </summary>
-        public bool IsMarkNegativeNumbers { get; set; }
-
-        /// <summary>
-        /// АктивизироватьПоУмолчанию.
-        /// </summary>
-        public bool ActivetedByDefault { get; set; }
 
         /// <summary>
         /// МногострочныйРежим.
         /// </summary>
         public bool InMultiLineMode { get; set; }
+
+        /// <summary>
+        /// Маска.
+        /// </summary>
+        public string Mask { get; set; }
 
         /// <summary>
         /// РежимПароля.
@@ -51,11 +34,6 @@ namespace OFP.ObjectModel.FormElements.Data
         public bool IsExtendedEdit { get; set; }
 
         /// <summary>
-        /// АвтоПереносСтрок.
-        /// </summary>
-        public bool IsWrapText { get; set; }
-
-        /// <summary>
         /// МинимальноеЗначение.
         /// </summary>
         public decimal MinValue { get; set; }
@@ -66,14 +44,29 @@ namespace OFP.ObjectModel.FormElements.Data
         public decimal MaxValue { get; set; }
 
         /// <summary>
-        /// Маска.
+        /// АвтоОтметкаНезаполненного.
         /// </summary>
-        public string Mask { get; set; }
+        public bool AutoMarkBlank { get; set; }
 
         /// <summary>
-        /// ГоризонтальноеПоложение.
+        /// АвтоВыборНезаполненного.
         /// </summary>
-        public HorizontalAlign HorizontalAlign { get; set; }
+        public bool AutoChoiceBlank { get; set; }
+
+        /// <summary>
+        /// ВыбиратьТип.
+        /// </summary>
+        public bool IsChooseType { get; set; }
+
+        /// <summary>
+        /// АвтоПереносСтрок.
+        /// </summary>
+        public bool IsWrapText { get; set; }
+
+        /// <summary>
+        /// ВыделятьОтрицательные.
+        /// </summary>
+        public bool IsMarkNegativeNumbers { get; set; }
 
         /// <summary>
         /// ВертикальноеПоложение.
@@ -81,9 +74,19 @@ namespace OFP.ObjectModel.FormElements.Data
         public VerticalAlign VerticalAlign { get; set; }
 
         /// <summary>
+        /// ГоризонтальноеПоложение.
+        /// </summary>
+        public HorizontalAlign HorizontalAlign { get; set; }
+
+        /// <summary>
         /// Оформление поля ввода.
         /// </summary>
         public TextBoxDecoration Decoration { get; set; }
+
+        /// <summary>
+        /// АктивизироватьПоУмолчанию.
+        /// </summary>
+        public bool ActivetedByDefault { get; set; }
 
         /// <summary>
         /// События поля ввода.
