b73c7c4c60f5caea8c7554f775fd96e53f5e7e2e
ebugusey <ebugusey@users.noreply.github.com>
2020-05-10 03:12:39 +0400

    build: Добавить команду для регенерации парсера
    При разработке listener-ов нужно постоянно дорабатывать грамматику
    парсера и после этого регенерировать парсер.
    
    Чтобы не заставлять разработчика устанавливать ANTLR и указывать
    кучу параметров, в которых он может допустить ошибку, вручную,
    добавлен target в Nuke.

--- build/Build.cs
+++ build/Build.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Nuke.Common;
 using Nuke.Common.Execution;
@@ -8,6 +10,7 @@ using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Tools.GitVersion;
+using Nuke.Common.Utilities;
 using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
@@ -77,4 +80,35 @@ class Build : NukeBuild
 
     Target CI => _ => _
         .DependsOn(Clean, Compile, Test);
+
+    [PathExecutable]
+    readonly Tool Java;
+    AbsolutePath Antlr => RootDirectory / "antlr" / "antlr-4.8-complete.jar";
+    AbsolutePath Grammar => RootDirectory / "grammar" / "OrdinaryForm.g4";
+    AbsolutePath ParserPath => SourceDirectory / "OFP.Library" / "Parser" / "Generated";
+    const string ParserNamespace = "OFP.Parser.Generated";
+
+    Target Regen => _ => _
+        .Executes(() =>
+        {
+            var env = new Dictionary<string, string>
+            {
+                ["CLASSPATH"] = Antlr,
+            };
+
+            var args = new[]
+            {
+                "-jar", Antlr,
+                "-o", ParserPath,
+                "-listener",
+                "-package", ParserNamespace,
+                "-Dlanguage=CSharp",
+                Path.GetFileName(Grammar),
+            };
+
+            Java(
+                args.Select(x => x.DoubleQuoteIfNeeded()).JoinSpace(),
+                Grammar.Parent,
+                env);
+        });
 }
